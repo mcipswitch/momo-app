f456b566233cbc1b339bdf2fb3f813699bd970fd
Priscilla Ip <priscillaip@Priscillas-MacBook-Pro-2.local>
2020-11-18 00:42:37 -0500

    Create cascade animation for journal list

--- MomoApp/MomoApp/Navigation/MomoJournalView/JournalListView.swift
+++ MomoApp/MomoApp/Navigation/MomoJournalView/JournalListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct JournalListView: View {
 
     @ObservedObject var viewModel = EntriesViewModel(dataManager: MockDataManager())
+    @State var animate: Bool = false
 
     var layout: [GridItem] {
         [GridItem(.flexible())]
@@ -20,9 +21,19 @@ struct JournalListView: View {
             LazyVGrid(columns: layout) {
                 ForEach(viewModel.entries.indices) { index in
                     EntryView(entry: viewModel.entries[index])
+                        .opacity(animate ? 1 : 0)
+                        .animation(Animation
+                                    .easeInOut(duration: 0.8)
+                                    .delay(Double(index) * 0.05)
+                        )
                 }
             }
             .padding()
+        }
+        .onAppear {
+            withAnimation(Animation.default.delay(1.0)) {
+                self.animate.toggle()
+            }
         }
     }
 }
