9c4eb0c9336cb53f949ea7c28a0b551510f0fbda
Priscilla Ip <priscillaip@Priscillas-MacBook-Pro-2.local>
2020-12-04 09:27:17 -0500

    Fix drag and tap gesture to work simultaneously

--- MomoApp/MomoApp/Navigation/MomoJournalView/MiniGraphView.swift
+++ MomoApp/MomoApp/Navigation/MomoJournalView/MiniGraphView.swift
@@ -22,8 +22,6 @@ struct MiniGraphView: View {
     @State var idxSelection: Int = 6
     @State private var indexShift: Int = 0
     @State private var newIndex: Int = 6
-    
-    var date = Date()
 
     // Selection Line
     @GestureState private var dragState: DragState = .inactive
@@ -31,6 +29,8 @@ struct MiniGraphView: View {
     @State private var dragOffset: CGFloat = 0
     @State var value: CGFloat = 0.5
     @State var opacity: Bool = false
+
+    var date = Date()
 
     // MARK: - Body
 
@@ -40,9 +40,6 @@ struct MiniGraphView: View {
         //let combined = pressGesture.sequenced(before: dragGesture)
 
         ZStack {
-            LineGraphView(dataPoints: self.dataPoints)
-                .padding()
-
             GeometryReader { geo in
 
                 // Calculate the spacing between graph lines
@@ -52,67 +49,76 @@ struct MiniGraphView: View {
                 let totalItemWidth: CGFloat = itemWidth * numOfItems
                 let itemFrameSpacing: CGFloat = (geo.size.width - totalItemWidth) / numOfSpaces
                 let itemSpacing: CGFloat = itemWidth + itemFrameSpacing
-                let columnLayout: [GridItem] = Array(repeating: .init(.flexible(), spacing: itemFrameSpacing),
-                                                     count: self.entries.count)
+                let columnLayout: [GridItem] = Array(
+                    repeating: .init(.flexible(), spacing: itemFrameSpacing),
+                    count: self.entries.count)
 
                 LazyVGrid(columns: columnLayout, alignment: .center) {
                     ForEach(0 ..< self.entries.count) { idx in
-                        GraphLine(
-                            idxSelection: self.idxSelection,
-                            newIdx: self.newIndex,
-                            idx: idx,
-                            entries: self.entries
-                        )
-                        .frame(minWidth: itemWidth, idealHeight: geo.size.height, maxHeight: geo.size.height)
 
-                        // Make whole stack tappable
-                        .contentShape(Rectangle())
-
-                        // TODO: - make this gesture work with the drag gesture
-                        .onTapGesture {
-                            self.changeIdxSelection(to: idx)
-                        }
-                        .overlayPreferenceValue(SelectionPreferenceKey.self, { preferences in
-                            SelectionLine(preferences: preferences)
-                                .opacity(self.opacity ? 1 : 0)
-                                .offset(x: self.currentOffset)
-
-                                // TODO: - make this work alongside tap gesture
-                                .gesture(
-                                    DragGesture(minimumDistance: 0)
-                                        .onChanged { value in
-                                            self.dragOffset = value.translation.width
-
-                                            // Calculate the index shift to the closest entry
-                                            self.indexShift = Int(round(value.translation.width / itemSpacing))
-                                            let newOffset = itemSpacing * CGFloat(indexShift)
-                                            self.currentOffset = newOffset
-
-                                            // Protect from scrolling out of bounds
-                                            self.newIndex = self.idxSelection + self.indexShift
-                                            self.newIndex = max(0, self.newIndex)
-                                            self.newIndex = min(self.entries.count - 1, self.newIndex)
-                                        }
-                                        .updating($dragState) { value, state, transaction in
-                                            state = .active(location: value.location, translation: value.translation)
-                                        }
-                                        .onEnded { value in
-                                            self.currentOffset = 0
-                                            self.idxSelection = newIndex
-                                            self.dragOffset = .zero
-                                        }
-                                )
-                        })
-                        .onReceive(self.viewRouter.objectWillChange, perform: {
-                            // Animate in selection line after graph line animation
-                            withAnimation(self.viewRouter.isHome
-                                            ? Animation.linear.delay(0.2)
-                                            : Animation.easeInOut(duration: 1.0).delay(1.8)) {
-                                self.opacity.toggle()
+                        ZStack {
+                            GraphLine(
+                                idxSelection: self.idxSelection,
+                                newIdx: self.newIndex,
+                                idx: idx,
+                                entries: self.entries
+                            )
+                            .frame(minWidth: itemWidth, idealHeight: geo.size.height, maxHeight: geo.size.height)
+                            .border(Color.yellow.opacity(0.2))
+                            .onTapGesture {
+                                self.changeIdxSelection(to: idx)
                             }
-                        })
+                            .overlayPreferenceValue(SelectionPreferenceKey.self, { preferences in
+                                SelectionLine(preferences: preferences)
+                                    .opacity(self.opacity ? 1 : 0)
+                                    .offset(x: self.currentOffset)
+
+                                    // TODO: - make this work alongside tap gesture
+                                    .gesture(
+                                        DragGesture(minimumDistance: 0)
+                                            .onChanged { value in
+                                                self.dragOffset = value.translation.width
+
+                                                // Calculate the index shift to the closest entry
+                                                self.indexShift = Int(round(value.translation.width / itemSpacing))
+                                                let newOffset = itemSpacing * CGFloat(indexShift)
+                                                self.currentOffset = newOffset
+
+                                                // Protect from scrolling out of bounds
+                                                self.newIndex = self.idxSelection + self.indexShift
+                                                self.newIndex = max(0, self.newIndex)
+                                                self.newIndex = min(self.entries.count - 1, self.newIndex)
+                                            }
+                                            .updating($dragState) { value, state, transaction in
+                                                state = .active(location: value.location, translation: value.translation)
+                                            }
+                                            .onEnded { value in
+                                                self.currentOffset = 0
+                                                self.idxSelection = newIndex
+                                                self.dragOffset = .zero
+                                            }
+                                    )
+                            })
+                            .onReceive(self.viewRouter.objectWillChange, perform: {
+                                // Animate in selection line after graph line aniamtes in
+                                withAnimation(self.viewRouter.isHome
+                                                ? Animation.linear.delay(0.2)
+                                                : Animation.easeInOut(duration: 0.8).delay(1.8)) {
+                                    self.opacity.toggle()
+                                }
+                            })
+
+
+                        }
+
+
                     }
                 }
+
+                LineGraphView(dataPoints: self.dataPoints)
+                    .padding(EdgeInsets(top: 16, leading: 12, bottom: 16, trailing: 12))
+                    .allowsHitTesting(false)
+
                 VStack {
                     Text("IDX Selection: \(self.idxSelection)")
                     Text("Drag: \(self.dragOffset)")
@@ -172,18 +178,16 @@ struct GraphLine: View {
                 .onChange(of: newIdx) { idx in
                     self.on = true
                 }
-            VStack(spacing: 8) {
-                Text("\(self.entries[idx].date.weekday)")
-                    .momoText(.graphWeekday)
-                Text("\(self.entries[idx].date.day)")
-                    .momoText(.graphDay)
-            }
+            dateLabel
         }
+        // This is needed to make whole stack tappable
+        .contentShape(Rectangle())
     }
 
     var line: some View {
         Rectangle()
             .foregroundColor(.clear).frame(width: 1)
+
             .background(
                 LinearGradient(
                     gradient: Gradient(colors: [
@@ -193,6 +197,15 @@ struct GraphLine: View {
                     startPoint: .bottom,
                     endPoint: .top)
             )
+    }
+
+    var dateLabel: some View {
+        VStack(spacing: 8) {
+            Text("\(self.entries[idx].date.weekday)")
+                .momoText(.graphWeekday)
+            Text("\(self.entries[idx].date.day)")
+                .momoText(.graphDay)
+        }
     }
 }
 
